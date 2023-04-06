df0890dcd0f66926f9e673046d15a186ea0038fd
LenouvelLouis <117534107+LenouvelLouis@users.noreply.github.com>
2023-04-06 12:29:57 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BanerComponent } from './baner/baner.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PersonnalInformationComponent } from './personnal-information/personnal-information.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BanerComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    ContactComponent,
+    SkillsComponent,
+    PersonnalInformationComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/personnal-information/personnal-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnal-information',
+  templateUrl: './personnal-information.component.html',
+  styleUrls: ['./personnal-information.component.css']
+})
+export class PersonnalInformationComponent {
+
+}
